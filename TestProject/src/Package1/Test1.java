89c1cc0de17f6973fb26039a23790a4b3a77f6e8
Sai321-me <68238030+Sai321-me@users.noreply.github.com>
2020-08-02 18:09:26 +0530

    Added changes in Class Test1.java

--- TestProject/src/Package1/Test1.java
+++ TestProject/src/Package1/Test1.java
@@ -1,10 +1,35 @@
 package Package1;
 
+import java.io.File;
+import java.io.IOException;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BiffException, IOException {
 		// TODO Auto-generated method stub
+		File f= new File("./data/Testingsheet.xls");//important
+		Workbook w = Workbook.getWorkbook(f);
+		Sheet s = w.getSheet("Sheet1");
+		int Columns = s.getColumns();
+		int rows = s.getRows();
+		System.out.println(Columns+"  "+rows);
 
-	}
+		String inputData [] []= new String [rows] [Columns];
+		for (int i=0; i<rows;i++ ) {
+			for(int j=0; j<Columns; j++) {
+				Cell c =s.getCell(j,i);
+				inputData[i][j]= c.getContents();
+				System.out.println(inputData[i][j]);
+			}
+		}
+		  
 
-}
+			}
+
+		}
+	
